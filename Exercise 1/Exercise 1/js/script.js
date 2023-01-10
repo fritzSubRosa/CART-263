5ce021ddd8dbd24839110282e5b8eec8992fd639
fritzSubRosa <122397099+fritzSubRosa@users.noreply.github.com>
2023-01-10 14:57:22 -0500

    Reorganized and created the green background for template of exercise 1

---
+++ Exercise 1/Exercise 1/js/script.js
@@ -0,0 +1,34 @@
+/**
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+    createCanvas(500,500);
+    background(0,255,0);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
