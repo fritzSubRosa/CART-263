721b60cc8d133b589d8f3248ccbc3f28c4121338
fritzSubRosa <122397099+fritzSubRosa@users.noreply.github.com>
2023-03-07 01:48:03 -0500

    Premiere League Dataset 1.0
    Main upload of Premiere League Dataset visualization, version 1.0

--- Project 2/template-p5-project/js/script.js
+++ Project 2/template-p5-project/js/script.js
@@ -7,6 +7,9 @@ Stephen
 
 let table;
 let players = [];
+let slideNumber = 0;
+let newOrder = 1;
+let wonderkids = [];
 
 function preload() {
     table = loadTable("premStats.csv","csv","header");
@@ -15,10 +18,11 @@ function preload() {
 
 function setup() {
     createCanvas(1000,1000);
-    background(50);
+    background(53, 159, 1);
+    drawPitch();
     for (let i = 0; i<table.getRowCount();i++){
-        players[i] = new PremPlayer(table.getString(i,0),  // Team Name
-                                    table.get(i,1),        // Jersey Number
+        players[i] = new PremPlayer(table.getString(i,0),   // Team Name
+                                    table.get(i,1),         // Jersey Number
                                     table.getString(i,2),   // Player Name
                                     table.getString(i,3),   // Player Position
                                     table.get(i,4),         // Appearances (Starts)
@@ -27,18 +31,18 @@ function setup() {
                                     table.get(i,7),         // Penalty Goals
                                     table.get(i,8),         // Yellow Cards
                                     table.get(i,9),         // Red Cards
-                                    table.get(i,10))        // Team Code
+                                    table.get(i,10),        // Team Code
+                                    table.get(i,11))        // Player Code
         }
+    print(players);
 }
 
 function draw() {
-    for(let i = 0; i<table.getRowCount();i++){
-        players[i].draw();
-    }
+    
 }
 
 class PremPlayer {
-    constructor(team,number,name,position,appearances,subs,goals,pens,yellows,reds,code){
+    constructor(team,number,name,position,appearances,subs,goals,pens,yellows,reds,code,playercode){
         this.team = team;
         this.number = number;
         this.name = name;
@@ -50,83 +54,298 @@ class PremPlayer {
         this.yellows = yellows;
         this.reds = reds;
         this.code = code;
+        this.order = playercode;
         this.primaryColor = color(0);
         this.secondaryColor = color (0);
         this.tertiaryColor = color(0);
+        this.textColor = color(0);
         this.striped = false;
-        this.x = random(width);
-        this.y = random(height);
-        this.diameter = 50;
-        this.strokeWeight = 10;
+        this.x = 25;
+        this.y = height+50;
+        this.xIncrement = 0;
+        this.yIncrement = 0;
+        this.displacement = 45;
+        this.upDisplacement = 25;
+        this.diameter = 15;
+        this.strokeWeight = 3;
+        this.numberSize = 10;
+        this.newOrder = 0;
 
         if (this.team == "Arsenal"){
             this.primaryColor = color(245,0,0);
             this.secondaryColor = color(255);
-            this.drawPlayer();
-            
+            this.textColor = color(255);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
         }
         else if (this.team == "Aston Villa"){
             this.primaryColor = color(133,1,28);
             this.secondaryColor = color (164,201,249);
-            this.drawPlayer();
-        }
+            this.textColor = color(255);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
+            
+        } 
         else if (this.team == "Brentford"){
             this.primaryColor = color(249,0,0);
             this.secondaryColor = color(255);
             this.tertiaryColor = color(0);
+            this.textColor = color (0);
             this.striped = true;
-            this.drawPlayer();
-        }
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
+        } 
         else if (this.team == "Brighton and Hove Albion"){
             this.primaryColor = color(0,0,255);
             this.secondaryColor = color(255);
             this.tertiaryColor = color(240,185,4);
             this.striped = true;
-            this.drawPlayer();
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
         }
         else if (this.team == "Burnley"){
             this.primaryColor = color(129,192,255);
             this.secondaryColor = color(129,1,26);
-            this.drawPlayer();
+            this.textColor = color (255);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
         }
         else if (this.team == "Chelsea"){
             this.primaryColor = color (0,0,255);
             this.secondaryColor = color (255);
-            this.drawPlayer();
+            this.textColor = color(255);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
         }
         else if (this.team == "Crystal Palace"){
             this.primaryColor = color(212,24,29);
             this.secondaryColor = color(27, 77, 173);
-            this.drawPlayer();
+            this.textColor = color(255);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
         }
         else if (this.team == "Everton"){
             this.primaryColor = color(13, 48, 122);
             this.secondaryColor = color(22, 85, 164);
-            this.drawPlayer();
+            this.textColor = color(255);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
         }
         else if (this.team == "Leeds United"){
             this.primaryColor = color (255);
             this.secondaryColor = color (209, 220, 30);
-            this.drawPlayer();
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
         }
-       
+        else if (this.team == "Leicester City"){
+            this.primaryColor = color(25,70,155);
+            this.secondaryColor = color(238, 178, 30);
+            this.textColor = color(255);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
+        }
+        else if (this.team == "Liverpool"){
+            this.primaryColor = color (209, 4, 37);
+            this.secondaryColor = color (248, 152, 104);
+            this.textColor = color(255);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
+        }
+        else if (this.team == "Manchester City"){
+            this.primaryColor = color(105, 190, 239);
+            this.secondaryColor = color (255);
+            this.textColor = color(255);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
+        }
+        else if (this.team == "Manchester United"){
+            this.primaryColor = color(226, 39, 46);
+            this.secondaryColor = color(255, 230, 0);
+            this.textColor = color(255);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
+        }
+        else if (this.team == "Newcastle United"){
+            this.primaryColor = color(255);
+            this.secondaryColor = color(0);
+            this.tertiaryColor = color(0);
+            this.textColor = color(255,0,0);
+            this.striped = true;
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
+        }
+        else if (this.team == "Norwich City"){
+            this.primaryColor = color(251, 214, 1);
+            this.secondaryColor = color(1, 139, 20);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
+        }        
+        else if (this.team == "Southampton"){
+            this.primaryColor = color(255);
+            this.tertiaryColor = color(0);
+            this.secondaryColor = color(242, 38, 47);
+            this.striped = true;
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
+        }
+        else if (this.team == "Tottenham Hotspur"){
+            this.primaryColor = color (255);
+            this.secondaryColor = color (34, 42, 71);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
+        }
+        else if (this.team == "Watford"){
+            this.primaryColor = color (229, 211, 67);
+            this.secondaryColor = color (0);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
+        }
+        else if (this.team == "West Ham United"){
+            this.primaryColor = color (192, 216, 228);
+            this.secondaryColor = color (117, 22, 44);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
+        }
+        else if (this.team == "Wolverhampton Wanderers"){
+            this.primaryColor = (0);
+            this.secondaryColor = color (249, 203, 48);
+            this.textColor = color(249, 203, 48);
+            this.xIncrement = this.code*this.displacement;
+            this.yIncrement = height-this.order*this.upDisplacement;
+        }
     }
     drawPlayer(){
         if (this.striped == false){
             strokeWeight(this.strokeWeight);
             stroke(this.secondaryColor);
             fill(this.primaryColor);
-            circle(this.x,this.y,this.diameter)
+            circle(this.x+this.xIncrement,this.y-this.yIncrement,this.diameter)
+            textAlign(CENTER);
+            noStroke();
+            fill(this.textColor);
+            textSize(this.numberSize);
+            text (this.number,this.x+this.xIncrement,this.y+5-this.yIncrement);
         } else {
             strokeWeight(this.strokeWeight);
             stroke(this.tertiaryColor);
             fill(this.primaryColor)
-            circle(this.x,this.y,this.diameter)
+            circle(this.x+this.xIncrement,this.y-this.yIncrement,this.diameter)
             rectMode(CENTER);
             noStroke();
             fill(this.secondaryColor);
-            rect(this.x,this.y,(this.diameter/4),this.diameter*.75)
+            rect(this.x+this.xIncrement,this.y-this.yIncrement,(this.diameter/4),this.diameter*.80)
+            textAlign(CENTER);
+            noStroke();
+            fill(this.textColor);
+            textSize(this.numberSize);
+            text (this.number,this.x+this.xIncrement,this.y+5-this.yIncrement);
+        }
+    }
+    redCards() {
+        if (this.reds != 0){
+            rectMode(CENTER);
+            fill(255,0,0);
+            rect(this.x+this.xIncrement+(this.diameter*.4),this.y-this.yIncrement-(this.diameter*.4),5,10);
+        }
+    }
+    goalsScored() {
+        if (this.goals != 0){
+            strokeWeight(1);
+            stroke(0);
+            fill(255);
+            ellipse(this.x+this.xIncrement-(this.diameter*.4),this.y-this.yIncrement-(this.diameter*.4),10);
+        }
+    }
+    singleAppearance() {
+        if(this.appearances ==1 || (this.appearances == 0 && this.subs == 1)){ 
+            this.drawPlayer();
+            this.redCards();
+            this.goalsScored();
+        }
+    }
+    exceptionalCases(){
+        if(this.appearances == 1 && (this.goals > 0 || this.reds > 0)){
+            print("found somebody!");
+            this.x = width/6;
+            this.y = height+100;
+            this.newOrder = newOrder;
+            newOrder++;
+            this.upDisplacement = 100;
+            this.diameter = 25;
+            this.numberSize = 16;
+            this.xIncrement = 0;
+            this.yIncrement = height-this.newOrder*this.upDisplacement;
+            print(this.yIncrement);
+            this.drawPlayer();
+            this.redCards();
+            this.goalsScored();
+            fill(0);
+            noStroke();
+            textSize(24);
+            textAlign(LEFT);
+            text (this.name, this.x+this.xIncrement+ 100, this.y-this.yIncrement);
+            text (this.team, this.x+this.xIncrement+ 100, this.y-this.yIncrement+30);
+            text ("Goals: "+this.goals, this.x+this.xIncrement+300,this.y-this.y-this.yIncrement);
+            text ("Red Cards: "+this.reds, this.x+this.xIncrement+300,this.y-this.y-this.yIncrements+30);
+        }
+    }
+}
+
+function mouseReleased(){
+    slideNumber++;
+    if (slideNumber == 1){
+        print("Creating players!");
+        for (var k = 0;k<players.length;k++){
+            print("running timer!")
+            players[k].drawPlayer();
+        }
+        print(slideNumber);
+        print(slideNumber);
+    }
+     if(slideNumber == 2){
+        print("Issuing cards");
+        for(var k = 0;k<players.length;k++){
+            print("Giving em out!");
+            players[k].redCards();
         }
     } 
-
+    if(slideNumber ==3){
+        print("Scoring goooooooooaaaals!");
+        for(var k = 0;k<players.length;k++){
+            print("goooooooooool");
+            players[k].goalsScored();
+        }
+    }
+    if(slideNumber == 4){
+        print("Getting subbed off!");
+        drawPitch();
+        for(var k = 0;k<players.length;k++){
+            players[k].singleAppearance();
+        }
+    }
+    if(slideNumber == 5){
+        print("Narrowing down!")
+        drawPitch();
+        for(var k = 0;k<players.length;k++){
+            players[k].exceptionalCases();
+        }
+    }
 }
+
+function drawPitch(){
+    background(53, 159, 1);
+    stroke(255);
+    strokeWeight(10);
+    fill(53,159,1);
+    rectMode(CENTER);
+    rect(width/2,height/2,700,900); // Pitch lines
+    circle(width/2,height/2,150)    // Midfield circle
+    line(150,500,850,500); // Midfield line
+    circle(width/2,225,150);
+    circle(width/2,775,150);
+    rect(width/2,150, 450,200);
+    rect(width/2,850, 450,200);
+    rect(width/2,90, 250,75);
+    rect(width/2,910, 250,75);
+}
+
