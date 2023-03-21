1c2fb08ef5d90b9e8cd75a804c423a73d42512cb
fritzSubRosa <122397099+fritzSubRosa@users.noreply.github.com>
2023-03-21 14:47:54 -0400

    Test upload of script
    I said what I said

--- Morse Code Project/js/script.js
+++ Morse Code Project/js/script.js
@@ -1,33 +1,58 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
 "use strict";
 
 
-/**
-Description of preload
-*/
+let letterTimer = 0;
+let storedTime = 0;
+let symbol;
+let recording = false;
+let awaitingSymbol = true;
+let symbolArray=[];
+let letterArray=[];
+let wordTimer = 0;
+
 function preload() {
 
 }
 
-
-/**
-Description of setup
-*/
 function setup() {
-
+    createCanvas(1000,1000);
+    background(0);
+    frameRate(30);
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+    if(recording == true){
+        recordSymbol();
+    }
+    if(awaitingSymbol == false){
+        decodeLetter();
+    }
+}
+function mousePressed(){
+    recording = true;
+    print("Recording!");
+    fill(0,255,0);
+    ellipse(height/2,width/2,25);
+}
 
+function mouseReleased(){
+    recording = false;
+    storedTime = timer;
+    timer = 0;
+    if(storedTime > 30){
+        append(symbolArray, "Dash");
+    }
+    else {
+        append(symbolArray, "Dot");
+    }
+    storedTime = 0;
+    print(symbolArray);
+}
+
+function decodeLetter(){
+    
+}
+
+function recordSymbol(){
+    letterTimer++;
 }
